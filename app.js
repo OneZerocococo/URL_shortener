6457e382f5fd526db82fef40bb3c4945eeae3e7a
OneZerocococo <ylflyfly@gmail.com>
2022-10-18 17:30:39 +0800

    feat: set routes

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const routes = require('./routes')
 require('./config/mongoose')
 
 // set view engine
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(routes)
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
+
 
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
